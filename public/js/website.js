d6d31b2e145abe335cd5141a1048d6ebe3e13b21
thinhdao16 <92748487+thinhdao16@users.noreply.github.com>
2024-10-30 08:40:08 +0700

    ađa

---
+++ public/js/website.js
@@ -0,0 +1,61 @@
+/*var domain=window.location.protocol+"//"+window.location.host+"/";
+
+$(document).mouseup(function(e)
+{
+	var container=$(".div_search");
+	if(!container.is(e.target)&&container.has(e.target).length===0)
+	{
+		$('#gh-search').removeClass('actives');
+		$('.idon-show').fadeIn(1000);
+		$('ul.wpc-menu>li>a').removeClass('search-f');
+	}
+});
+
+
+
+
+
+   
+   
+    
+
+    
+
+
+	
+	$(document).ready(function(){
+ 
+    // hide #back-top first
+    $(".back-to-top").hide();
+ 
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('.back-to-top').fadeIn();
+            } else {
+                $('.back-to-top').fadeOut();
+            }
+        });
+ 
+        // scroll body to 0px on click
+        $('.back-to-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+  });
+
+
+
+
+
+
+   
+
+
+
+
+*/
